cda1a56bd78dd9ffaa07b1668a7684b82874746c
demente <olga.agen@gmail.com>
2021-01-15 08:55:10 +0100

    add components

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Burger from "./../../components/Burger/Burger";
+class BuilderBuilder extends Component {
+    render() {
+        return (
+            <>
+                <Burger />
+                <div>Build Controls</div>
+            </>
+        );
+    }
+}
+
+export default BuilderBuilder;
